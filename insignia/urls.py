e3e74cbf01d448279ae82ff0f0e9bd744ca9a8ff
eferdman <gnudevliz@gmail.com>
2017-04-12 18:07:50 -0700

    wire in basic ajax request

--- insignia/urls.py
+++ insignia/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^columns/(?P<table_id>[0-9]+)/$', views.edit_columns, name='edit_columns'),
-    url(r'^table/(?P<table_id>[0-9]+)/$', views.table_view, name='table_view')
+    url(r'^table/(?P<table_id>[0-9]+)/$', views.table_view, name='table_view'),
+    url(r'^ajax/validate/$', views.validate, name='validate')
 ]
 
